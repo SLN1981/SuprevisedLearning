06bfce69048e3ea6d17468f41f3dd058b55dd71a
SLN1981 <narasimhan.ronaldinho007@gmail.com>
2025-09-14 15:55:24 +0530

    Recommender System Content Based Filtering

---
+++ UnSuprevisedLearning/Week2/Content Based Filtering/Assignment.py
@@ -0,0 +1,27 @@
+import numpy as np
+import numpy.ma as ma
+import pandas as pd
+import tensorflow as tf
+from tensorflow import keras
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.model_selection import train_test_split
+import tabulate
+from recsysNN_utils import *
+pd.set_option("display.precision", 1)
+
+
+# GRADED_FUNCTION: sq_dist
+# UNQ_C2
+def sq_dist(a,b):
+    """
+    Returns the squared distance between two vectors
+    Args:
+      a (ndarray (n,)): vector with n features
+      b (ndarray (n,)): vector with n features
+    Returns:
+      d (float) : distance
+    """
+    ### START CODE HERE ###     
+    d = np.sum(np.square(a - b))
+    ### END CODE HERE ###     
+    return d
